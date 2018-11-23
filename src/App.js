cb56f232acf90b8045a7245abdcccd39108177f2
Yulong Tan <yulongtan67@gmail.com>
2018-11-23 14:55:27 -0800

    Add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import {HeaderY} from './headers/Headers';
 
 class App extends Component {
   render() {
     return (
-      <div>Fresh Start!</div>
+      <HeaderY title='Looking for entertainment?' subtitle='Find music, movies, books, and more of your favorite genre.' />
     )
   }
 }
